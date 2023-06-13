46fc6bfb7aea8a499483b2b69d85906d7663d69b
shreerampoudel <ccrpoudelalt@gmail.com>
2023-06-13 14:28:35 +0545

    Resturant_Menu_App

---
+++ README.md
@@ -0,0 +1,6 @@
+## Screenshots
+- <img src="img/login_page.jpg" alt="Login Page" width="300"> - <img src="img/drawer.jpg" alt="Drawer" width="300">
+- <img src="img/description.jpg" alt="Description" width="300"> - <img src="img/menu.jpg" alt="Menu" width="300">
+
+
+
---
+++ lib/CartPage.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class CartPage extends StatelessWidget {
+  const CartPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      backgroundColor: Color.fromARGB(255, 230, 47, 47),
+    body: Center(child: Text("COMING SOON!!!",style:TextStyle(fontSize: 40,),)),
+    );
+  }
+}
